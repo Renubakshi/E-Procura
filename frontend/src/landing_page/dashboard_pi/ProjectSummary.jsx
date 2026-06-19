28cdb509e38eaba38ff335fc533822e694dc485a
Renu <coolrey1512>
2026-06-19 11:43:16 +0530

    backend debugging

--- frontend/src/landing_page/dashboard_pi/ProjectSummary.jsx
+++ frontend/src/landing_page/dashboard_pi/ProjectSummary.jsx
@@ -2,7 +2,6 @@ import { useSearchParams, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import BackButton from "../../components/BackButton";
 import { API_URL, axiosInstance } from "../../config/api";
-import signedBadge from "../../assets/signed-badge.jpg"
 
 export default function ProjectSummary() {
   const { id } = useParams();
@@ -155,7 +154,7 @@ window.open(fileURL, "_blank");
 
       <div className="flex items-center bg-green-50 border border-green-300 mt-6 rounded-lg shadow-sm">
         <img
-          src={signedBadge}
+          src="/assets/signed-badge.jpg"
           alt="Signed Badge"
           className="h-20 object-contain"
         />
